1fb187e73d2ec67c29cebef3174e3759ca94775f
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2025-04-08 17:41:22 +0200

    Fix broken copy button

--- build_static_site.py
+++ build_static_site.py
@@ -643,10 +643,10 @@ def generate_example_html(
                             <path d="M16 1H4C2.9 1 2 1.9 2 3V17H4V3H16V1ZM19 5H8C6.9 5 6 5.9 6 7V21C6 22.1 6.9 23 8 23H19C20.1 23 21 22.1 21 21V7C21 5.9 20.1 5 19 5ZM19 21H8V7H19V21Z" fill="currentColor"/>
                         </svg>
                         Copy All Python
-                    </button>
+                    </button>"""
+                    + f"""
                     <div id="all-python-code" style="display: none;">{escape(all_python_code)}</div>
-                </div>
-""")
+                </div>""")
 
         # Close header container
         f.write("""            </div>
